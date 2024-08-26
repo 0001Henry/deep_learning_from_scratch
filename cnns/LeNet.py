2fbd35153b72126c4fe53f1d015254bac7d8f5ba
0001Henry <22281067@bjtu.edu.cn>
2024-08-26 13:16:31 +0800

    update

--- cnns/LeNet.py
+++ cnns/LeNet.py
@@ -23,9 +23,57 @@ class LeNet5(nn.Module):
         return y
     
 
+class LeNetplus(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            # 增加卷积层的深度和通道数
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),  # b*1*28*28 -> b*32*28*28
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),  # b*32*28*28 -> b*64*28*28
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # -> b*64*14*14
+            nn.BatchNorm2d(64),  # 增加 Batch Normalization
+            
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),  # b*64*14*14 -> b*128*14*14
+            nn.ReLU(),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),  # b*128*14*14 -> b*256*14*14
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # -> b*256*7*7
+            nn.BatchNorm2d(256),  # 增加 Batch Normalization
+            
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),  # b*256*7*7 -> b*512*7*7
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),  # b*512*7*7 -> b*512*7*7
+            nn.ReLU(),
+            # nn.MaxPool2d(kernel_size=2, stride=2),  # -> b*512*3*3
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=0), # -> b*512*3*3
+            nn.BatchNorm2d(512),  # 增加 Batch Normalization
+            
+            nn.Flatten(),  # -> b*4608
+            nn.Linear(512 * 3 * 3, 1024), 
+            nn.ReLU(),
+            nn.Dropout(0.5),  # 增加 Dropout
+            nn.Linear(1024, 512), 
+            nn.ReLU(),
+            nn.Dropout(0.5),  # 增加 Dropout
+            nn.Linear(512, 256), 
+            nn.ReLU(),
+            nn.Linear(256, 10)  # -> b*10
+        )
+        
+    def forward(self, x):
+        return self.net(x)
+
+
+
 if __name__ == '__main__':
     x = torch.randn(64, 1, 28, 28)
-    model = LeNet5()
+    model = LeNetplus()
     y = model(x)
     print(y.shape)
-    torch.onnx.export(model,x,"./onnx/LeNet5.onnx")
+    
+    # 计算模型参数量
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f'Total number of parameters: {total_params}')
+    # torch.onnx.export(model,x,"./onnx/LeNet5.onnx")
